47dd17fbba511034140a547120476a7804cefc5e
alycia hall <amhall25@gmail.com>
2019-09-18 19:11:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,6 +4,13 @@ hello_t(array) do |name|
     puts "Hi, #{name}"
   end
 end
+def hello_t(array)
+  i = 0
+
+  while i < array.length
+    yield array[i]
+    i = i + 1
+  end
 end
 
 # call your method here!
