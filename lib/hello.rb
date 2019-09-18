92bcc9b9e1ce2040b591d4cc40a664715fe92a2b
alycia hall <amhall25@gmail.com>
2019-09-18 19:17:16 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,8 +1,18 @@
+# def hello_t(array)
+#hello_t(array) do |name|
+#  if name.start_with?("T")
+#    puts "Hi, #{name}"
+#  end
+#end
 def hello_t(array)
-hello_t(array) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
+  i = 0
+
+  while i < array.length
+    yield(array[i])
+    i = i + 1
   end
+
+  array
 end
 
 
